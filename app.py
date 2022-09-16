d411c91da05c15d9f06489486cee22f9dcc77449
acs106104 <acs106104@gm.ntcu.edu.tw>
2022-09-16 16:53:05 +0800

    Update app.py
    New test

--- app.py
+++ app.py
@@ -41,8 +41,9 @@ def handle_message(event):
 
 @handler.add(BeaconEvent)
 def handle_beacon(event): 
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=f"Beacon 通知！"))
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=f"偵測如下："))
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text="Got beacon event. hwid="+ event.beacon.hwid +"device_message(hex string)="+ event.beacon.dm))
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=f"Beacon 通知！\n"+
+    "Got beacon event. hwid="+ event.beacon.hwid +"device_message(hex string)="+ event.beacon.dm))
+    # line_bot_api.reply_message(event.reply_token, TextSendMessage(text=f"偵測如下："))
+    # line_bot_api.reply_message(
+    #     event.reply_token,
+    #     TextSendMessage(text="Got beacon event. hwid="+ event.beacon.hwid +"device_message(hex string)="+ event.beacon.dm))
