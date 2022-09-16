bc0301a46b7bb5ba9e6586f09c7e045c8bab705b
acs106104 <acs106104@gm.ntcu.edu.tw>
2022-09-16 22:12:12 +0800

    Update app.py
    adds flex message

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import os
 from datetime import datetime
+import json
 
 from flask import Flask, abort, request
 
 # https://github.com/line/line-bot-sdk-python
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage, BeaconEvent
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, BeaconEvent, FlexSendMessage
 
 app = Flask(__name__)
 
@@ -36,16 +37,53 @@ def handle_message(event):
     get_message = event.message.text
 
     # Send To Line
-    reply = TextSendMessage(text=f"Reply: {get_message}")
+    reply = TextSendMessage(text=f"Echo: {get_message}")
     line_bot_api.reply_message(event.reply_token, reply)
 
 @handler.add(BeaconEvent)
 def handle_beacon(event):   
     hwid = event.beacon.hwid
-    dm = event.beacon.dm
-    reply = TextSendMessage(text=f"Beacon 新通知！\nGot beacon event. hwid= {hwid}\ndevice_message(hex string)= {dm}")
+    # reply = TextSendMessage(text=f"Beacon 新通知！\nGot beacon event. hwid= {hwid}")
+    reply = FlexSendMessage(
+    alt_text='hello',
+    contents={
+        "type": "bubble",
+        "direction": "ltr",
+        "header": {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+            {
+                "type": "text",
+                "text": "Beacon 通知",
+                "weight": "bold",
+                "color": "#000000FF",
+                "align": "center",
+                "gravity": "center",
+                "contents": []
+            }
+            ]
+        },
+        "hero": {
+            "type": "image",
+            "url": "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/3e7653db-f3c4-40c9-9f32-c31d7447f7de/sketch-1531214280539.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45/20220916/us-west-2/s3/aws4_request&X-Amz-Date=20220916T132633Z&X-Amz-Expires=86400&X-Amz-Signature=6cc6f8d8b9e9208c3b4725681d0c9e9c3825799afd813718c352abc4dd757674&X-Amz-SignedHeaders=host&response-content-disposition=filename =\"sketch-1531214280539.png\"&x-id=GetObject",
+            "size": "full",
+            "aspectRatio": "1.51:1",
+            "aspectMode": "fit"
+        },
+        "body": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+        {
+            "type": "text",
+            "text": "接收到的 hwid: " + str(hwid),
+            "align": "center",
+            "contents": []
+        }
+        ]
+        }
+    }
+    
+)
     line_bot_api.reply_message(event.reply_token, reply)
-    # line_bot_api.reply_message(event.reply_token, TextSendMessage(text=f"偵測如下："))
-    # line_bot_api.reply_message(
-    #     event.reply_token,
-    #     TextSendMessage(text="Got beacon event. hwid="+ event.beacon.hwid +"device_message(hex string)="+ event.beacon.dm))
