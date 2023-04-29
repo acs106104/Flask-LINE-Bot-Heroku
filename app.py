fe08ecbfabc9d18cc76f06f54eea44b1311ffa6f
acs106104 <45888686+acs106104@users.noreply.github.com>
2023-04-29 16:30:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage, BeaconEve
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi(os.environ.get("CHANNEL_ACCESS_TOKEN"))
-handler = WebhookHandler(os.environ.get("CHANNEL_SECRET"))
+line_bot_api = LineBotApi(os.environ.get("LINE_CHANNEL_ACCESS_TOKEN"))
+handler = WebhookHandler(os.environ.get("LINE_CHANNEL_SECRET"))
 
 
 @app.route("/", methods=["GET", "POST"])
